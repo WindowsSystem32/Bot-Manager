e1a19f5978db6e592e133313d84bd69ef9facb56
WindowsSystem32 <wincom811@naver.com>
2023-01-11 13:02:54 +0900

    Update Script.js

--- Script.js
+++ Script.js
@@ -200,7 +200,8 @@ function response(room, msg, sender, isGroupChat, replier, imageDB, packageName)
               delete sL[sender];
               res5 = true;
             } catch(e) {
-              replier.reply((res5? aI[2] : aI[3]) + "'" + msg + "'(으)로부터 스크립트를 복제" + (res5? "했" : "하지 못했") + "습니다.");
+              replier.reply((res5? aI[2] : aI[3]) + "'" + msg + "'(으)로부터 스크립트를 복제" + (res5? "했" : "하지 못했") + "습니다: " + ln + 
+              e);
             }
           } else {
             replier.reply(aI[4] + "스크립트를 먼저 선택해 주세요!");
